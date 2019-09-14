7967f88d775e97b1b1ef6788087b321dae5a703e
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2019-09-13 23:24:18 -0300

    Adding Overdraft Activation screen
    Signed-off-by: João Gabriel de Matos <joaogabriel.c.matos@gmail.com>

--- kotlin-project/app/src/main/java/com/eps/creditoffer/MainActivity.kt
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.eps.creditoffer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            val intent = Intent(this, TelaDeConfirmacaoChequeEspecial::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/TelaDeConfirmacaoChequeEspecial.kt
@@ -0,0 +1,12 @@
+package com.eps.creditoffer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaDeConfirmacaoChequeEspecial : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_de_confirmacao_cheque_especial)
+    }
+}

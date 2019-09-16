527892057d8267d86eaa4ad1d7c4bbe1442a8d50
Gabriel Lopes <biel.lr2000@outlook.com>
2019-09-16 19:09:01 -0300

    Adding credit type choice screen
    Signed-off-by: Gabriel Lopes <biel.lr2000@outlook.com>

--- kotlin-project/app/src/main/java/com/eps/creditoffer/MainActivity.kt
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/MainActivity.kt
@@ -11,9 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn.setOnClickListener {
-            val intent = Intent(this, TelaDeConfirmacaoChequeEspecial::class.java)
-            startActivity(intent)
         }
     }
-}
+

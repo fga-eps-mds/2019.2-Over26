84c1024ba33a9b8878ecc250bd918a007ef4b532
Erick Reis <erickcorreareis@gmail.com>
2019-09-17 11:33:34 -0300

    Adjusting files for pull request.
    Signed-off-by: Erick Reis <erickcorreareis@gmail.com>

--- kotlin-project/app/src/main/java/com/eps/creditoffer/TrackLimit.kt
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/TrackLimit.kt
@@ -2,19 +2,10 @@ package com.eps.creditoffer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.track_limit.*
 import android.widget.SeekBar
-import com.marcinmoskala.arcseekbar.ArcSeekBar
-import com.marcinmoskala.arcseekbar.ProgressListener
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 
-
-
-class MainActivity : AppCompatActivity() {
+class TrackLimit : AppCompatActivity() {
 
     var curLimit: Float = 0F
     var maxLimit: Float = 200F
@@ -22,23 +13,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.track_limit)
 
         text_view_usage.setText("R$ " + curUsage.toInt().toString())
         text_view_usage2.setText("R$0,00")
         text_view_cur.setText("R$ " + curLimit.toInt().toString())
         text_view_max.setText("R$ " + maxLimit.toInt().toString())
         seek_bar.max = maxLimit.toInt()
-
-        //Log.d(arc_bar.onProgressChangedListener.toString())
-
-        //arc_bar.maxProgress = 200
-        //val progressListener: ProgressListener// = { progress -> Log.i("SeekBar", "Value is $progress") }
-        //progressListener.invoke(0)
-        //arc_bar.onProgressChangedListener = progressListener
-
-
-
 
         // Set a SeekBar change listener
         seek_bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {

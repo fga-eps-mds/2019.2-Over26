09e907fe71e23edf7cf313174515a5dba575152e
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2019-09-20 17:29:22 -0300

    Adding overdraft post request on the frontend
    Co-authored-by: João Paulo Coelho <japapauloxd@hotmail.com>

--- kotlin-project/app/src/main/java/com/eps/creditoffer/MainActivity.kt
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.eps.creditoffer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        /*Botão auxiliar q será removido posteriormente*/
+        val auxiliar = findViewById(R.id.auxiliar) as Button
+
+        auxiliar.setOnClickListener{
+
+            val intent = Intent(this, eligibilidade::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
 
         }
     }
--- kotlin-project/app/src/main/java/com/eps/creditoffer/eligibilidade.kt
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/eligibilidade.kt
@@ -1,12 +1,48 @@
 package com.eps.creditoffer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kittinunf.result.Result
+import android.widget.Button
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.fuel.Fuel
+//mport com.github.kittinunf.fuel.android.core.Json
+//import com.github.kittinunf.fuel.android.extension.responseJson
+import com.github.kittinunf.fuel.core.FuelError
+
+
 
 class eligibilidade : AppCompatActivity() {
+    val url: String = "10.0.2.2:3000/api/overdrafts"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_eligibilidade)
+        println("1")
+
+       val eligibility_button = findViewById(R.id.eligibility_button) as Button
+        eligibility_button.setOnClickListener {
+
+
+            url.httpPost().responseString { request, response, result ->
+                    when (result) {
+                        is Result.Success -> {
+                            //val data = result.get()
+                            //println(data)
+                            val intent = Intent(this, TelaDeConfirmacaoChequeEspecial::class.java)
+
+                        }
+                        is Result.Failure -> {
+                            println("Deu ruim")
+                        }
+
+                    }
+                }
+
+
+        }
+        }
     }
-}
+

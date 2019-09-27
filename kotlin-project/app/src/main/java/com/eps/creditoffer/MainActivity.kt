8038c65ad7667a02da6d86bb983fc1d096a530f7
Erick Reis <erickcorreareis@gmail.com>
2019-09-27 16:09:42 -0300

    Movies fragment examples.
    Signed-off-by: Erick Reis <erickcorreareis@gmail.com>

--- kotlin-project/app/src/main/java/com/eps/creditoffer/MainActivity.kt
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.eps.creditoffer
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import androidx.annotation.LayoutRes
 
 class MainActivity : AppCompatActivity() {
+
+    private val layoutResId: Int
+        @LayoutRes
+        get() = R.layout.fragment_main
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,12 +18,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun mudarTela(view: View) {
-        Toast.makeText(this, "Show some text on the screen.", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.fragment_main)
 
-        val intent = Intent(this,MonthlyInstallment::class.java)
-        startActivity(intent)
+        val fm = supportFragmentManager
+        var fragment = fm.findFragmentById(R.id.fragment_container)
 
+        // ensures fragments already created will not be created
+        if (fragment == null) {
+            fragment = MainFragment.newInstance()
+            // create and commit a fragment transaction
+            fm.beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
-
 }
 
---
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/MainFragment.kt
@@ -0,0 +1,45 @@
+package com.eps.creditoffer
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_main.*
+
+data class Movie(val title: String, val year: Int, val image: String)
+
+class MainFragment : Fragment() {
+
+    private val nicCageMovies = listOf(
+        Movie("Raising Arizona", 1987, "raising_arizona.jpg"),
+        Movie("Vampire's Kiss", 1988, "vampires_kiss.png"),
+        Movie("Con Air", 1997, "con_air.jpg"),
+        Movie("Face/Off", 1997, "face_off.jpg"),
+        Movie("National Treasure", 2004, "national_treasure.jpg"),
+        Movie("The Wicker Man", 2006, "wicker_man.jpg"),
+        Movie("Bad Lieutenant", 2009, "bad_lieutenant.jpg"),
+        Movie("Kick-Ass", 2010, "kickass.jpg")
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_main, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        list_recycler_view.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = ListAdapter(nicCageMovies)
+        }
+    }
+
+    companion object {
+        fun newInstance(): MainFragment = MainFragment()
+    }
+}

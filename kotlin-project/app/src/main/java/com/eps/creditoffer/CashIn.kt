bb766fd79682f1f2cbff900e5d69db89c038a9b5
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2019-10-15 11:30:50 -0300

    Changing font's color to match prototipe.
    Co-authored-by: Luiza Cezario <luiza_cezario@hotmail.com>

--- kotlin-project/app/src/main/java/com/eps/creditoffer/CashIn.kt
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/CashIn.kt
@@ -2,11 +2,17 @@ package com.eps.creditoffer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_cash_in.*
 
 class CashIn : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cash_in)
+        val optionsString= arrayOf("","Boleto","Transferencia");
+        chashInSpinner.adapter=ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,optionsString)
+
     }
 }

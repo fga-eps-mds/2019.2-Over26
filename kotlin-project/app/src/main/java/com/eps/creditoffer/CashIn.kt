0f17b46f344bb85b255c88b9ec77e7f4a2e568a6
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2019-10-14 21:07:26 -0300

    Adding CashsIn activity.
    Co-authored-by: Luiza7 <luiza_cezario@hotmail.com>

---
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/CashIn.kt
@@ -0,0 +1,12 @@
+package com.eps.creditoffer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CashIn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cash_in)
+    }
+}

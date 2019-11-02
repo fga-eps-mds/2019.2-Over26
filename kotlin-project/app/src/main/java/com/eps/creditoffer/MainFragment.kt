e02cbbad20ed784cf6ccdfded7f515634ac69b05
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2019-11-01 23:42:39 -0300

    Removing non functional button of mainFragment.
    Co-authored-by: João Paulo Coelho <japapauloxd@hotmail.com>
    Co-authored-by: Erick Reis <erickcorreareis@gmail.com>

--- kotlin-project/app/src/main/java/com/eps/creditoffer/MainFragment.kt
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/MainFragment.kt
@@ -1,8 +1,10 @@
 package com.eps.creditoffer
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_main.*
@@ -40,4 +42,9 @@ class MainFragment : Fragment() {
     companion object {
         fun newInstance(): MainFragment = MainFragment()
     }
-}
+
+    /*fun close(view: View) {
+print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    }*/
+
+    }

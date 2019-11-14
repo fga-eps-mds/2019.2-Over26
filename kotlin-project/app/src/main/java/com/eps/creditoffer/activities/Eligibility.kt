b4ebfaad59a37cfcd4661366e061945a8d029c07
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2019-11-14 12:47:56 -0300

    Making changes to the 'get' methods to use currentUser's Id.
    Signed-off-by: João Gabriel de Matos <joaogabriel.c.matos@gmail.com>

--- kotlin-project/app/src/main/java/com/eps/creditoffer/activities/Eligibility.kt
+++ kotlin-project/app/src/main/java/com/eps/creditoffer/activities/Eligibility.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.eps.creditoffer.connections.OverdraftLink
 import com.eps.creditoffer.R
+import com.eps.creditoffer.utils.currentUser
 import kotlinx.android.synthetic.main.activity_eligibility.*
 
 class Eligibility : AppCompatActivity() {
@@ -18,8 +19,8 @@ class Eligibility : AppCompatActivity() {
         val overdraft = OverdraftLink()
 
         eligibility_button.setOnClickListener(View.OnClickListener {
-            if (!overdraft.get(1)) {
-                overdraft.create(1)
+            if (!overdraft.get(currentUser.id)) {
+                overdraft.create(currentUser.id)
             }
             val intent = Intent(this, OverdraftConfirmation::class.java)
             startActivity(intent)
